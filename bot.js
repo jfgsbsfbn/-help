aa1777459c19e05aeab73e703d4f6cb003114320
legend-boker <39490532+legend-boker@users.noreply.github.com>
2018-07-25 15:10:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -101,17 +101,6 @@ ${text}
           });
 
 
-on('message', msg =>{
-    let message=msg;
-    if(message.content.startsWith("bc")){
-        var args = message.content.split(' ').slice(1).join(' ');
-    msg.guild.members.forEach(m=>{
-        m.send(args.replace(/[user]/g,m)).catch();
-    if(message.attachments.first()){
-m.sendFile(message.attachments.first().url).catch();
-    }
-    })    ;
-    }
-});
+
 
 bot.login(process.env.BOT_TOKEN);
