74c34b6baa8c9009a431c44f005fe795663cf0b5
legend-boker <39490532+legend-boker@users.noreply.github.com>
2018-07-25 13:52:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,15 @@
-const Discord = require('discord.js');
-const db = require('quick.db');
-const Canvas = require('canvas');
-
-const client = new Discord.Client();   
-const googl = require('goo.gl');  
-const translate = require('google-translate-api');   
-const fs = require("fs"); 
-const getYoutubeID = require('get-youtube-id'); 
-const moment = require("moment");  
-const { Client, Util } = require('discord.js');  
-const UserBlocked = new Set(); 
-const jimp = require('jimp');   
-const math = require('math-expression-evaluator'); 
-const stripIndents = require('common-tags').stripIndents;
-const figlet = require('figlet');
-const google = require('google-it'); 
-const queue = new Map(); 
-const zalgo = require('zalgolize');   
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const ytdl = require('ytdl-core');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const sql = require("sqlite");
- const dateFormat = require('dateformat'); 
- const pretty = require('pretty-ms') 
-,ti={}  
-,spee={};
-
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const fs = require("fs");// npm i fs
+const moment = require("moment");// npm i fs
+var jimp = require('jimp');// npm i jimp 
+var Canvas = require('canvas');// npm i canvas
+   
 
 const prefix = '!'
 const mmss = require('ms');
+
+
 
 
                   client.on('message', message => {
@@ -63,7 +43,7 @@ const mmss = require('ms');
 
 
 
-on('ready', () => {
+bot.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Scrpit By ┃ LEGEND`);
         console.log(`----------------`);
